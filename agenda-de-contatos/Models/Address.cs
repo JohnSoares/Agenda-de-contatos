e2cfe28d794b4458eb582fec83c08356db89b3ca
John Soares <johnalisson.csoares@gmail.com>
2019-02-13 13:31:01 -0300

    Criação do projeto, e criação parcial da classe Users(Correção)

---
+++ agenda-de-contatos/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace agenda_de_contatos.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ agenda-de-contatos/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace agenda_de_contatos.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
